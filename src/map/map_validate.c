14c1467350b2bb67f39be4d50d3d40f58992b577
TRSTN4 <trstnvb4@gmail.com>
2024-03-19 00:29:55 +0100

    Add even more norminette

--- src/map/map_validate.c
+++ src/map/map_validate.c
@@ -6,36 +6,45 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/06 12:03:11 by trstn4        #+#    #+#                 */
-/*   Updated: 2024/03/18 18:37:25 by trstn4        ########   odam.nl         */
+/*   Updated: 2024/03/19 00:29:10 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
 
-// Checks if a character is allowed
-bool cub_is_char_in_here(char c) {
-    return c == '0' || c == '1' || c == 'N' || c == 'E' || c == 'W' || c == 'S' || c == '\n' || c == '\0';
-}
-
-// Removes white spaces and checks characters
-void cub_check_map_characters(t_map *map)
+int	cub_is_char_in_here(char c)
 {
-    for (int i = 0; i < map->height; i++) {
-        for (int j = 0; map->field[i][j] != '\0'; j++) {
-            // Skipping whitespace characters
-            if (map->field[i][j] == ' ' || map->field[i][j] == '\t') {
-                continue;
-            }
-
-            if (!cub_is_char_in_here(map->field[i][j])) {
-                printf("Error\nCharacter: '%c' is not allowed.\n", map->field[i][j]);
-                exit(1);
-            }
-        }
-    }
+	return (c == '0' || c == '1' || c == 'N' || c == 'E' || c == 'W' || c == 'S'
+		|| c == '\n' || c == '\0');
 }
 
-void cub_is_cub_extension(char *file)
+void	cub_check_map_characters(t_map *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (map->field[i][j] != '\0')
+		{
+			if (map->field[i][j] != ' ' && map->field[i][j] != '\t')
+			{
+				if (!cub_is_char_in_here(map->field[i][j]))
+				{
+					printf("Error\nCharacter: '%c' is not allowed.\n",
+						map->field[i][j]);
+					exit(1);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	cub_is_cub_extension(char *file)
 {
 	char	*extension;
 	int		path_len;
@@ -52,10 +61,10 @@ void cub_is_cub_extension(char *file)
 	}
 }
 
-int cub_validate_map(t_map *map, char *file)
+int	cub_validate_map(t_map *map, char *file)
 {
 	t_check_map	*check_map;
-	
+
 	cub_is_cub_extension(file);
 	check_map = ft_calloc(1, sizeof(t_check_map));
 	cub_setup_map_checks(map, check_map);
